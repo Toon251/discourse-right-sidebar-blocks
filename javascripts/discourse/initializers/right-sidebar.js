291e98add98c25a916f2900b6b6579718fb52e7e
Toon251 <toon251@gmail.com>
2025-04-03 08:04:40 +0000

    Change path of component

--- javascripts/discourse/initializers/right-sidebar.js
+++ javascripts/discourse/initializers/right-sidebar.js
@@ -1,5 +1,5 @@
 import { apiInitializer } from 'discourse/lib/api';
-import TcRightSideBar from '../components/tc-subscription-bar';
+import TcRightSideBar from '../../connectors/tc-right-sidebar';
 
 export default apiInitializer('1.14.0', (api) => {
     api.renderInOutlet(settings.plugin_outlet.trim(), TcRightSideBar);
