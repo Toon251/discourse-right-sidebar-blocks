ee89f327765dac1755d456e0f41294b2bdc97a41
Toon251 <toon251@gmail.com>
2025-04-03 08:09:34 +0000

    Remove initializer

--- javascripts/discourse/initializers/right-sidebar.js
+++
@@ -1,6 +0,0 @@
-import { apiInitializer } from 'discourse/lib/api';
-import TcRightSideBar from '../../connectors/tc-right-sidebar';
-
-export default apiInitializer('1.14.0', (api) => {
-    api.renderInOutlet(settings.plugin_outlet.trim(), TcRightSideBar);
-});
